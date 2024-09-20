853576d2f1da70e89824e8d5337b14ab90fa0e80
JUANI\juani <juanisabbatini@gmail.com>
2024-09-20 14:22:13 -0300

    change port

--- app.py
+++ app.py
@@ -162,4 +162,4 @@ def predict():
 if __name__ == '__main__':
     # Get the port from the environment (useful for deployment), otherwise default to 5000
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port | 5000)
