7a1e01f664c9504b931944d03ab0446902faa4f9
JUANI\juani <juanisabbatini@gmail.com>
2024-09-20 14:46:24 -0300

    delete port

--- app.py
+++ app.py
@@ -160,7 +160,4 @@ def predict():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    print('Starting Flask API server port:', port)
-    # Get the port from the environment (useful for deployment), otherwise default to 5000
-    app.run(debug=True, host='0.0.0.0', port=port | 5000)
+    app.run()
